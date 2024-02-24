cbd47d94ac54c43244ac41d4c75fd7315d1a17ee
Tan dev <tandev151>
2024-02-24 09:42:34 +0700

    build app

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,89 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        appBar: AppBar(title: Text('Quiz App')),
+        body: QuizPage(),
       ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  const QuizPage({super.key});
+
+  @override
+  State<QuizPage> createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeepers = [];
+  // List<Map<String, String>> questions = [
+  //   {'What?': 'True'},
+  //   {'question': 'Where?', 'anwser': 'True'},
+  //   {'question': 'What?', 'anwser': 'True'},
+  //   {'question': 'What?', 'anwser': 'True'},
+  //   {'question': 'What?', 'anwser': 'True'},
+  //   {'question': 'What?', 'anwser': 'True'},
+  // ];
+
+  List<String> questions = ['What', 'Where'];
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Expanded(
+            flex: 5,
+            child: Center(
+              child: Text(
+                questions[0],
+                style: TextStyle(),
+              ),
+            )),
+        Expanded(
+            flex: 1,
+            child: MaterialButton(
+              minWidth: double.infinity,
+              child: Text(
+                'True',
+                style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white),
+              ),
+              onPressed: () {
+                print('Clicked!');
+                setState(() {
+                  scoreKeepers.add(Icon(Icons.check));
+                });
+              },
+              color: Colors.green[400],
+            )),
+        Expanded(
+            flex: 1,
+            child: MaterialButton(
+              minWidth: double.infinity,
+              onPressed: () {
+                print('Clicked!');
+                setState(() {
+                  scoreKeepers.add(Icon(Icons.close));
+                });
+              },
+              child: Text(
+                'False',
+                style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white),
+              ),
+              color: Colors.red[400],
+            )),
+        Expanded(
+            child: Row(
+          children: scoreKeepers,
+        )),
+      ],
     );
   }
 }
